d9b9c23b81e4db9f3e6a939b6d4195e802bd4840
Kyrylo Golikov <pipecc1985@gmail.com>
2025-01-06 18:37:06 +0100

    make build

---
+++ index.74c0024d.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),c=(e.clientWidth-t.clientWidth)/2,l=(e.clientHeight-t.clientHeight)/2;t.style.transform="translateX(".concat(c,"px) translateY(").concat(l,"px)");
+//# sourceMappingURL=index.74c0024d.js.map
