5201c5c86f05f9d8ad05273a7144dbb9b0a1d63b
Kyrylo Golikov <pipecc1985@gmail.com>
2025-01-06 18:35:05 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,9 @@
 'use strict';
 
 // write code here
-const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const offsetX = (wall.clientWidth - spider.clientWidth) / 2;
+const offsetY = (wall.clientHeight - spider.clientHeight) / 2;
 
-const wallRect = wall.getBoundingClientRect();
-
-const spiderCenterX = wallRect.width / 2 - spider.offsetWidth / 2;
-const spiderCenterY = wallRect.height / 2 - spider.offsetHeight / 2;
-
-
-spider.style.left = `${spiderCenterX}px`;
-spider.style.top = `${spiderCenterY}px`;
+spider.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px)`;
